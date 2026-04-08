0ce95f64a7b41b9e6c7e00cc235496c64241dfe1
gilsonmarcondes <gilson.mcm@hotmail.com>
2026-04-08 12:26:26 -0300

    Add files via upload

--- api_google.py
+++ api_google.py
@@ -1,4 +1,3 @@
-# Arquivo: api_google.py
 import requests
 import streamlit as st
 
@@ -8,16 +7,25 @@ def buscar_lugares_google(query, chave_google):
     if not query or len(query) < 3: 
         return {}
         
-    url = f"https://maps.googleapis.com/maps/api/place/textsearch/json?query={query}&key={chave_google}&language=pt-BR"
+    url = "https://maps.googleapis.com/maps/api/place/textsearch/json"
+    
+    # A MÁGICA ACONTECE AQUI: Deixamos o 'requests' formatar a URL
+    parametros = {
+        "query": query,
+        "key": chave_google,
+        "language": "pt-BR"
+    }
     
     try:
-        res = requests.get(url, timeout=10).json()
+        res = requests.get(url, params=parametros, timeout=10).json()
+        
         if res.get('status') == 'OK':
             return {item['formatted_address']: item['geometry']['location'] for item in res['results']}
         elif res.get('status') == 'ZERO_RESULTS':
             return {}
         else:
-            st.warning(f"Atenção (Google): {res.get('status')}")
+            st.warning(f"Atenção (Google): {res.get('status')} - Verifique se a Places API está ativada no console.")
+            
     except Exception as e:
         st.error(f"Erro na busca de endereço: {e}")
         
